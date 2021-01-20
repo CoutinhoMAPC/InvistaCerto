efc47a408e9f0033b4eb1a43ff7c6b9c8dd292d7
CoutinhoMAPC <marcosandrepennacoutinho@gmail.com>
2021-01-20 16:12:57 -0300

    primeira página

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+//componente react de função que contem a nossa página padrão
+function Home() {
+	return <div> Home </div>
+}
+
+export default Home //exporta o componente React como padrão de página para o render
